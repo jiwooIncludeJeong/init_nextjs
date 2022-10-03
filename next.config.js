d1ab9943e1a639d4cf48e2116875c0b299a73494
jeongjiwoo-include <jjw89506343@gmail.com>
2022-10-03 21:47:29 +0900

    [dev] change path to locate under src and add recoil

--- next.config.js
+++ next.config.js
@@ -2,6 +2,30 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  extends: [
+    'plugin:@next/next/recommended',
+  ],
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.(js|ts)x?$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
+  async headers() {
+    return [{
+      source: '/(.*)',
+      headers: [{
+        key: 'X-Frame-Options',
+        value: 'DENY'
+      }]
+    }]
+  }
 }
 
 module.exports = nextConfig
