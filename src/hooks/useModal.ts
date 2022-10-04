8318ac60f1bb007c035658074ab7999e2c6d9a3c
jeongjiwoo-include <jjw89506343@gmail.com>
2022-10-04 13:38:23 +0900

    [dev] add hook test library

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,3 +1,21 @@
-const useModal = () => {};
+import { useState } from 'react';
+
+const useModal = (initialValue?: boolean) => {
+  const [show, setShow] = useState<boolean>(initialValue ?? false);
+
+  const open = () => {
+    setShow(true);
+  };
+
+  const close = () => {
+    setShow(false);
+  };
+
+  return {
+    show,
+    open,
+    close,
+  };
+};
 
 export default useModal;
